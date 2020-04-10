6010ad0c1d4f1f349fca3c370c8531e5baf4118d
Shyam Jindhe Aswathanarayana <mkxsam@gmail.com>
2020-04-10 17:09:56 +0100

    Changes to logger and enabling the clean and smudge filter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const API_KEY="asdfasdfasdfaerw24323423"
 
 const makeAPICall = function(key) {
-    console.log("Making the API Call1 ")
+    console.log("Making the API Call3 ")
 }
 
 makeAPICall(API_KEY)
